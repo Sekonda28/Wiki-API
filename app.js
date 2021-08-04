0b66d9880784d17fefb82a6d5dc6616fa3092d1f
Matt Caswell <matt.caswell@hotmail.com>
2021-08-04 15:18:16 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
+
+//TODO
+mongoose.connect("mongodb://localhost:27017/wikiDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+mongoose.set('useFindAndModify', false);
+
+const articleSchema = {
+    title: String,
+    content: String
+};
+
+const Article = mongoose.model("Article", articleSchema);
+
+
+
+app.listen(3000, function () {
+    console.log("Server started on port 3000");
+});
