4ee38d38a8eb34ce57611452d033d5f6ebb17144
Matt Caswell <matt.caswell@hotmail.com>
2021-08-05 17:07:46 +0200

    add patch, put, delete functions

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+const _ = require("lodash");
 
 const app = express();
 
@@ -25,6 +26,92 @@ const articleSchema = {
 };
 
 const Article = mongoose.model("Article", articleSchema);
+
+app.route("/articles")
+    .get(function (req, res) {
+        Article.find({}, function (err, foundArticles) {
+            if (!err) {
+                res.send(foundArticles);
+            } else {
+                res.send(err);
+            }
+        });
+    })
+    .post(function (req, res) {
+
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content
+        })
+        newArticle.save(function (err) {
+            if (!err) {
+                res.send("Successfully added a new article")
+            } else {
+                res.send(err)
+            }
+        });
+    })
+    .delete(function (req, res) {
+        Article.deleteMany(function (err) {
+            if (!err) {
+                res.send("Successfully deleted all articles.");
+            } else {
+                res.send(err);
+            }
+        });
+    });
+
+// Requests targeting specific article
+app.route("/articles/:articleTitle")
+    .get(function (req, res) {
+        Article.findOne({
+            title: req.params.articleTitle
+        }, function (err, foundArticle) {
+            if (foundArticle) {
+                res.send(foundArticle)
+            } else {
+                res.send("No articles matching that title found")
+            }
+        })
+    })
+    .put(function (req, res) {
+        Article.replaceOne({
+                title: req.params.articleTitle
+            }, {
+                title: req.body.title,
+                content: req.body.content
+            },
+            function (err) {
+                if (!err) {
+                    res.send("Successfully updated article.")
+                }
+            }
+        )
+    })
+    .patch(function (req, res) {
+        Article.updateOne({
+                title: req.params.articleTitle
+            }, req.body,
+            function (err) {
+                if (!err) {
+                    res.send("Successfully updated article.")
+                } else {
+                    res.send(err)
+                }
+            }
+        )
+    })
+    .delete(function (req, res) {
+        Article.deleteOne({
+            title: req.params.articleTitle
+        }, function (err) {
+            if (!err) {
+                res.send("Successfully deleted article.")
+            } else {
+                res.send(err)
+            }
+        })
+    });
 
 
 
